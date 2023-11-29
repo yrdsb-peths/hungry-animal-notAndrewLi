e205e380bfb4e84a4f92139a1b0e9f6f8dc06e04
Andrew <andrew.li2252@gmail.com>
2023-11-29 13:07:36 -0500

    Background + Sound Effect
    Fixed the shrek swamp background and added a sound effect when a plane is eaten.

--- Frog.java
+++ Frog.java
@@ -12,10 +12,10 @@ public class Frog extends Actor
      * Act - do whatever the Frog wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootSound bite = new GreenfootSound("sounds/bite.mp3");
     public void act()
     {
         // Add your action code here.
-
         if(Greenfoot.isKeyDown("d")){
 
             move(5);
@@ -31,6 +31,7 @@ public class Frog extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createPlane();
             world.increaseScore();
+            bite.play();
         }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -13,14 +13,17 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    GreenfootImage background = new GreenfootImage("images/shrekSwamp");
+    
     
     public int score = 0;
     Label scoreLabel;
+    GreenfootImage background = new GreenfootImage("images/shrekSwamp.jpg");
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1, false); 
+        super(600, 400, 1, false);
+        background.scale(super.getWidth(), super.getHeight());
+        super.setBackground(background);
         //create the frog actor
         Frog frog = new Frog();
         addObject (frog, 300, 350);
