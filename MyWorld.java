3da690ca532d3e6c83d69826998789c82d588d7e
Andrew <andrew.li2252@gmail.com>
2023-11-29 12:36:20 -0500

    Problem with background
    Cannot find an image

--- MyWorld.java
+++ MyWorld.java
@@ -13,6 +13,8 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    GreenfootImage background = new GreenfootImage("images/shrekSwamp");
+    
     public int score = 0;
     Label scoreLabel;
     public MyWorld()
@@ -20,8 +22,6 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false); 
         //create the frog actor
-        
-        GreenfootImage background = myWorld 
         Frog frog = new Frog();
         addObject (frog, 300, 350);
         
