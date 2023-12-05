b4af90543f2671451ea0ff5449d94f483241260e
Andrew <andrew.li2252@gmail.com>
2023-12-05 12:52:40 -0500

    Problem with TitleScreen

---
+++ TitleScreen.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (Andrew Li) 
+ * @version December 25
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Frog", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        
+    }
+    /*
+     * the main world act loop
+     */
+    public void act(){
+        if(Greenfoot.isKeyDown("space")){
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
